2eabd5e9879c1f5c3ca9635805ca0382ca673d80
EHANG <uehang@gmail.com>
2014-11-13 17:37:28 +0800

    add some basic functions

--- phaddr.c
+++ phaddr.c
@@ -6,3 +6,16 @@
  ************************************************************************/
 
 #include "phaddr.h"
+
+int ph_load_data(PhAddr pa, char *data_file_name)
+{
+	int res = 0;
+	char buf[64];
+	int fd = open(data_file_name,O_RDONLY);
+	if(fd < 3){
+		printf("open file \"%s\" failed: %s", data_file_name, stderror(errno));
+		return 0;
+	}
+	
+	return res;
+}
--- phaddr.h
+++ phaddr.h
@@ -7,7 +7,11 @@
 #ifndef _PHADDR_H
 #define _PHADDR_H
 
-#include <stdio.h>
+#include <stdio.h>   // for printf()
+#include <fcntl.h>   // for open()
+#include <sys/stat.h>// for model O_RDONLY|O_RDWR|O_WRONLY
+#include <unistd.h>  // for close(), read(), write()
+#include <errno.h>   // for errno, use stderror(errno)
 
 #define INDEX_L1_STR "130"
 #define INDEX_L2_STR "0000"
@@ -15,19 +19,33 @@
 #define INDEX_L2_CNT 10000
 
 typedef void * pVoid;
-
-struct addr_node{
-	char ph_head[8];
-	char addr[40];
-}
+typedef struct ph_addr PhAddr;
+typedef struct addr_node AddrNode;
 
 struct ph_addr{
-	pVoid index_l1[INDEX_L1_CNT];
+	void *index_l1[INDEX_L1_CNT];
+}
+struct addr_node{
+	char phone_head[12];
+	char address[52];
+}
 
 
 /*
+ * load data:
+ * return 0: none data
+ *       >0: the count of data
+ */
+int ph_load_data(PhAddr pa, char *data_file_name);
+
+/*
  *
+ */
 int ph_insert_node(PhAddr pa, AddrNode an);
 
+/*
+ *
+ */
 void ph_get_addr(PhAddr pa, char *phone, char *addr);
+
 #endif
